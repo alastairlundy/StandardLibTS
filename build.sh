dda4a0e72131b9ece84f9a133786e9ce1c04b1d2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-06-30 20:04:33 +0100

    fix issues and publish 0.4.1

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-tsc --target esnext --outfile dist/main.js --module system
+##rm -f dist/main.d.ts
+
+mkdir dist/ts
+
+mkdir dist/js
+
+cp -a lib/. dist/ts 
+
+tsc  --target esnext --outfile dist/main.js --module system
+
+mv dist/main.js dist/js/main.js
+
+mv dist/main.d.ts dist/js/main.d.ts
