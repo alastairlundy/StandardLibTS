5bee68ec326f512cfb9972960efc096477ec2e8c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-06 15:51:47 +0000

    add equals method and contains (and containsPair) methods

--- src/lib/HashMap.ts
+++ src/lib/HashMap.ts
@@ -128,10 +128,56 @@ import { KeyValuePair } from "./KeyValuePair";
     }
 
     /**
+     *
+     * @param key
+     */
+    public contains(key: K): boolean{
+        let found: boolean = false;
+
+        for(let k of this.toArray()){
+            if(k === key){
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    /**
+     *
+     * @param keyValuePair
+     */
+    public containsPair(keyValuePair: KeyValuePair<K, V>): boolean{
+        let found: boolean = false;
+
+        for(let k of this.toArray()){
+            if(k.equals(keyValuePair)){
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    /**
      * 
      * @returns 
      */
     public toArray() : Array<KeyValuePair<K, V>>{
         return this.internalList.toArray();
+    }
+
+    /**
+     *
+     * @param hashMap
+     */
+    public equals(hashMap: HashMap<K, V>): boolean{
+        let equality: boolean = true;
+
+        for(let i of hashMap.toArray()){
+            if(!this.containsPair(i)){
+                equality = false;
+            }
+        }
+
+        return equality;
     }
 }
