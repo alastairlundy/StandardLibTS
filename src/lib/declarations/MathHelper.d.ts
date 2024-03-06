449cfbbb7349afdd5e07f0b10245d430417bfa11
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-06 16:30:28 +0000

    add declaration for MathHelper

---
+++ src/lib/declarations/MathHelper.d.ts
@@ -0,0 +1,10 @@
+export = MathHelper;
+
+declare class MathHelper {
+
+    public root(value: number, power: number): number;
+
+    public calculateGeometricMean(numbers: number[]): number;
+    public calculateArithmeticMean(numbers: number[]): number;
+
+}
