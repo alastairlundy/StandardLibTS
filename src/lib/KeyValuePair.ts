81c3d91b2b42234fcd75f2918deaf45dc865b6da
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-06 14:49:28 +0000

    add equality checking method to KeyValuePair

--- src/lib/KeyValuePair.ts
+++ src/lib/KeyValuePair.ts
@@ -6,4 +6,8 @@ export class KeyValuePair<K, V>{
         this.key = key;
         this.value = value;
     }
+
+    public equals(keyValuePair: KeyValuePair<K, V>): boolean{
+        return (this.key === keyValuePair.key) && (this.value === keyValuePair.value);
+    }
 }
